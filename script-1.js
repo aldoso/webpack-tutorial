0e3eb8c788f11e72393bcf0cf1d233125d4e0198
aldoso <alexdobras@gmail.com>
2017-09-27 15:08:45 +0300

    addde some explainations

--- script-1.js
+++ script-1.js
@@ -3,3 +3,4 @@ alert(message)
 
 // go to terminal and type:
 // webpack script-1.js ./bundle
+// to create the file containing all the javascript code from all files (for the moment just script-1.js and script-2.js)
